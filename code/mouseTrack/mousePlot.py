932aa04405a5a98300237bc9b400f6b0e16e0126
aisaly <alexandraisaly@gmail.com>
2018-03-23 23:59:57 -0400

    graph created using example random data

--- code/mouseTrack/mousePlot.py
+++ code/mouseTrack/mousePlot.py
@@ -5,18 +5,24 @@ Created on Tue Mar 13 11:31:10 2018
 @author: Alexandra
 """
 import matplotlib.pyplot as plt
+import random
 import pandas as pd
 import seaborn as sns; sns.set(style="white", color_codes=True)
 import mouseClickAndLocation #to get the csv path
 
-def read_from_CSV(csvFileName):
-    df = pd.read_csv(mouseClickAndLocation.csvPath + csvFileName);
-    return df
+#FOR EXAMPLE DATA ONLY-----
+df = pd.DataFrame()
+df['x'] = random.sample(range(1, 100), 25)
+df['y'] = random.sample(range(1, 100), 25)
 
-# EXAMPLE
-df = read_from_CSV('mouseLoc.csv') #get the dataframe
+#END------
+
+#def read_from_CSV(csvFileName):
+ #   df = pd.read_csv(mouseClickAndLocation.csvPath + csvFileName);
+  #  return df
+
+#df = read_from_CSV('mouseLoc.csv') #get the dataframe
 pd.set_option('display.float_format', lambda x: '%.7f' % x) #make sure scientific notation is off
-print(df) #print the data frame
+#print(df) #print the data frame
 
-df.head()
-sns.jointplot(x="x", y="y", data=df)
+g = sns.jointplot("x", "y", data=df, kind = "kde", space=0)
