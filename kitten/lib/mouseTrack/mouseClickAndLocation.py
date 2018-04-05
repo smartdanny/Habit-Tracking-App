92bc3cd2e59195a276a9aa5f477fd6fd651bbdbf
James Safko <jsafko@ufl.edu>
2018-04-05 14:48:03 -0400

    Timestamps -> time difference

--- kitten/lib/mouseTrack/mouseClickAndLocation.py
+++ kitten/lib/mouseTrack/mouseClickAndLocation.py
@@ -1,5 +1,6 @@
 from pynput import mouse
 import os.path
+import csv as c
 import time
 import datetime
 
@@ -15,37 +16,52 @@ class MOUSETHREAD(mouse.Listener):
     def __init__(self):
         # initializes mouse.Listener
         super().__init__(on_move = self.on_move, on_click = self.on_click, on_scroll = self.on_scroll)
+        self.t = 0
+        self.x = 0
+        self.y = 0
+
 
         # initializes the recording booleans to false
         self.recordLoc = False
         self.recordClicks = False
         self.recordScroll = False
+        self.firstClickTime = 0
+        self.firstMoveTime = 0
 
-        self.t = 0
-        self.x = 0
-        self.y = 0
+        if os.path.exists(csvPath + 'mouseLoc.csv'):
+            with open(csvPath + 'mouseLoc.csv') as f:
+                reader = c.reader(f)
+                row1 = next(reader)
+                row2 = next(reader)
+                self.firstMoveTime = float(row2[0])
+
+        if os.path.exists(csvPath + 'mouseClicks.csv'):
+            with open(csvPath + 'mouseClicks.csv') as f:
+                reader2 = c.reader(f)
+                row1 = next(reader2)
+                row2 = next(reader2)
+                self.firstClickTime = float(row2[0])
 
     # called when mouse moves
     def on_move(self, x, y):
         self.x = x
         self.y = y
-        self.t = datetime.datetime.fromtimestamp(time.time())
         if self.recordLoc:
-            print('Pointer moved to {0}'.format((self.x, self.y)) + ' at ' + str(self.t))
-            # with open('somefile.txt', 'a') as the_file:
-            #     the_file.write('Hello\n')
-            self.write_csv('mouseLoc.csv', str(self.t)  + ', ' + str(self.x) + ', ' + str(self.y) + '\n')
+            self.t = time.time()
+            print('Pointer moved to {0}'.format((self.x, self.y)) + ' at ' + str(datetime.datetime.fromtimestamp(self.t)))
+            self.t = round((self.t - self.firstMoveTime), 7)
+            self.write_csv('mouseLoc.csv', str(self.t)  + ',' + str(self.x) + ',' + str(self.y) + '\n')
 
     # called when mouse clicks
     def on_click(self, x, y, button, pressed):
         self.x = x
         self.y = y
-        self.t = datetime.datetime.fromtimestamp(time.time())
+        self.t = round((time.time() - self.firstClickTime), 7)
         if self.recordClicks:
             if pressed:
-                words = str(self.t) + ', ' + 'p, ' + str(self.x) + ', ' + str(y)
+                words = str(self.t) + ',' + 'p,' + str(self.x) + ',' + str(y)
             else:
-                words = str(self.t) + ', ' + 'r, ' + str(self.x) + ', ' + str(y)
+                words = str(self.t) + ',' + 'r,' + str(self.x) + ',' + str(y)
             print(words)
             self.write_csv('mouseClicks.csv', words + '\n') # prints p if pressed and r if released
 
@@ -53,7 +69,7 @@ class MOUSETHREAD(mouse.Listener):
     def on_scroll(self, x, y, dx, dy):
         self.x = x
         self.y = y
-        self.t = time.time()
+        self.t = round(time.time(), 7)
         if self.recordScroll:
             print('Scrolled {0} at {1}'.format(
                 'down' if dy < 0 else 'up',
@@ -65,9 +81,11 @@ class MOUSETHREAD(mouse.Listener):
         # If the file doesn't exist, make proper column titles
         if not os.path.exists(csvPath + csv):
             if(csv == 'mouseLoc.csv'):
+                self.firstMoveTime = self.t
                 with open(csvPath + csv, 'a') as f:
                     f.write('Time,x,y\n')
             if(csv == 'mouseClicks.csv'):
+                self.firstClickTime = self.t
                 with open(csvPath + csv, 'a') as f:
                     f.write('Time,Pressed/Released,x,y\n')
 
